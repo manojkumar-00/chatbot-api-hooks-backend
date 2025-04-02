93ecc9cb3de6f192612bdd6f28a40615c710189b
Manoj Kumar <manojkumar.ibmce@gmail.com>
2025-04-02 13:15:19 +0530

    Fix: added console log for errors

--- src/middlewares/webhook.middleware.js
+++ src/middlewares/webhook.middleware.js
@@ -7,54 +7,55 @@ const {
 } = require("../controllers/sheets.controller");
 
 async function handleAllOperations(req, res) {
-  try {
-    const purpose = req?.query?.purpose;
-    const data = req?.query?.data;
+    try {
+      console.log("Called with params : ", req.query);
+      const purpose = req?.query?.purpose;
+      const data = req?.query?.data;
 
-    const parsedData = data && JSON.parse(data);
+      const parsedData = data && JSON.parse(data);
 
-    switch (purpose) {
-      case "fetchById":
-        req.params.id = parsedData?.id;
-        getDataById(req, res);
-        break;
+      switch (purpose) {
+        case "fetchById":
+          req.params.id = parsedData?.id;
+          getDataById(req, res);
+          break;
 
-      case "validateByEmail":
-        req.params.email = parsedData?.email;
-        findUserByEmail(req, res);
-        break;
+        case "validateByEmail":
+          req.params.email = parsedData?.email;
+          findUserByEmail(req, res);
+          break;
 
-      case "validateByPhone":
-        req.params.phone = parsedData?.email;
-        findUserByPhone(req, res);
-        break;
+        case "validateByPhone":
+          req.params.phone = parsedData?.email;
+          findUserByPhone(req, res);
+          break;
 
-      case "addRecord":
-        const userData = {
-          name: parsedData.studentName,
-          email: parsedData.studentEmail,
-          gender: parsedData.gender,
-          phone: parsedData.mobileNumber,
-          branch: parsedData.studentCourse,
-          year: parsedData.year,
-        };
+        case "addRecord":
+          const userData = {
+            name: parsedData.studentName,
+            email: parsedData.studentEmail,
+            gender: parsedData.gender,
+            phone: parsedData.mobileNumber,
+            branch: parsedData.studentCourse,
+            year: parsedData.year,
+          };
 
-        req.body = userData;
-        addData(req, res);
-        break;
-      default:
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-          success: false,
-          message: "Something went wrong",
-        });
+          req.body = userData;
+          addData(req, res);
+          break;
+        default:
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Something went wrong",
+          });
+      }
+    } catch (error) {
+      console.error(error.message);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
-  } catch (error) {
-    console.error(error.message);
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: "Something went wrong",
-    });
-  }
 }
 
 module.exports = {
